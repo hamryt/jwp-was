f98f2bebf3b6b314589bd8852d0cb539c6375d65
hamryt <rkdl0829@naver.com>
2022-08-17 00:34:36 +0900

    :sparkles: HttpRequest, HttpResponse 구현

---
+++ src/main/java/request/HttpRequest.java
@@ -0,0 +1,14 @@
+package request;
+
+public class HttpRequest {
+
+    private final RequestLine requestLine;
+    private final RequestHeader requestHeader;
+    private final RequestBody requestBody;
+
+    public HttpRequest(RequestLine requestLine, RequestHeader requestHeader, RequestBody requestBody) {
+        this.requestLine = requestLine;
+        this.requestHeader = requestHeader;
+        this.requestBody = requestBody;
+    }
+}
---
+++ src/main/java/request/HttpRequestParser.java
@@ -0,0 +1,44 @@
+package request;
+
+import org.springframework.web.context.request.RequestScope;
+import utils.IOUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class HttpRequestParser {
+
+    private HttpRequestParser() {
+        throw new AssertionError();
+    }
+
+    public static HttpRequest parse(BufferedReader bufferedReader) throws IOException {
+        RequestLine requestLine = new RequestLine(bufferedReader.readLine());
+        RequestHeader requestHeader = parseHeader(bufferedReader);
+        RequestBody requestBody = parseBody(requestHeader, bufferedReader);
+
+        return new HttpRequest(requestLine, requestHeader, requestBody);
+    }
+
+    private static RequestHeader parseHeader(BufferedReader bufferedReader) throws IOException {
+        RequestHeader requestHeader = new RequestHeader();
+        String header = bufferedReader.readLine();
+
+        while (!header.isEmpty()) {
+            requestHeader.add(header);
+            header = bufferedReader.readLine();
+        }
+
+        return requestHeader;
+    }
+
+    private static RequestBody parseBody(RequestHeader requestHeader, BufferedReader bufferedReader) throws IOException {
+        if (requestHeader.hasRequestBody()) {
+            String body = IOUtils.readData(bufferedReader, requestHeader.getContentLength());
+
+            return new RequestBody(body);
+        }
+
+        return new RequestBody("");
+    }
+}
