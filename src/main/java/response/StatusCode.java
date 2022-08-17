d07a9d4dee530decf23f9d8e6bf33c608ae0f914
hamryt <rkdl0829@naver.com>
2022-08-18 01:19:04 +0900

    :sparkles: add: not found 상태 추가

--- src/main/java/response/StatusCode.java
+++ src/main/java/response/StatusCode.java
@@ -8,7 +8,8 @@ package response;
 
 public enum StatusCode {
     OK(200, "OK"),
-    FOUND(302, "Found");
+    FOUND(302, "Found"),
+    NOT_FOUND(404, "Not Found");
 
     private final int code;
     private final String reasonPhrase;
