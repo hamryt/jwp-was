2894ab4b93e5993e51b34564d5649b51a579e531
hamryt <rkdl0829@naver.com>
2022-08-19 01:37:53 +0900

    :sparkles: sessionAttribute 생성

---
+++ src/main/java/webserver/session/SessionAttribute.java
@@ -0,0 +1,39 @@
+package webserver.session;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class SessionAttribute {
+
+    private final Map<String, Object> attribute = new HashMap<>();
+
+    public void setAttribute(String name, String value) {
+        attribute.put(name, value);
+    }
+
+    public Object getAttribute(String name) {
+        return attribute.get(name);
+    }
+
+    public void removeAttribute(String name) {
+        attribute.remove(name);
+    }
+
+    public void invalidate() {
+        attribute.clear();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SessionAttribute that = (SessionAttribute) o;
+        return Objects.equals(attribute, that.attribute);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(attribute);
+    }
+}
