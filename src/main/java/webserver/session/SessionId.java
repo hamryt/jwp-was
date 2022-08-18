04f8ef23dc16b860a1afb0e87eaf1901eed45f25
hamryt <rkdl0829@naver.com>
2022-08-19 01:34:23 +0900

    :sparkles: sessionId 생성

---
+++ src/main/java/webserver/session/SessionId.java
@@ -0,0 +1,26 @@
+package webserver.session;
+
+import java.util.Objects;
+
+public class SessionId {
+
+    private final String id;
+
+    public SessionId(SessionIdGenerator sessionIdGenerator) {
+        this.id = sessionIdGenerator.generate();
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SessionId sessionId = (SessionId) o;
+        return Objects.equals(id, sessionId.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
